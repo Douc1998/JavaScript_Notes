13ae49b7e611df5737c1122d0d0e1bc895ce7279
Douc1998 <cugdc@cug.edu.cn>
2022-01-20 22:30:39 +0800

    2021-01-20

--- test.js
+++ test.js
@@ -1,36 +1,5 @@
-
-let person1 = new Object();
-person1.name = 'douchen';
-person1.age = 23;
-person1.saySchool = function(){
-    console.log('I am from Wuhan University !');
-}
-console.log(person1.name); // douchen
-console.log(person1['age']); // 23
-let descriptorName = Object.getOwnPropertyDescriptor(person1, 'name');
-let descriptorAge = Object.getOwnPropertyDescriptor(person1, 'age');
-let descriptorSchool = Object.getOwnPropertyDescriptor(person1, 'saySchool');
-console.log(descriptorName);
-console.log(descriptorAge);
-console.log(descriptorSchool);
-
-let descriptor = Object.getOwnPropertyDescriptors(person1);
-console.log(descriptor);
-console.log('Mac test');
-/**
-{
-  name: {
-    value: 'douchen',
-    writable: true,
-    enumerable: true,
-    configurable: true
-  },
-  age: { value: 23, writable: true, enumerable: true, configurable: true },
-  saySchool: {
-    value: [Function (anonymous)],
-    writable: true,
-    enumerable: true,
-    configurable: true
-  }
-}
-*/
+const hoursMark = (function(){
+  let hours = [...Array(24)].map((item, index) => index + 1);
+  return hours.map(item => {return `${ item }时`})
+}())
+console.log(hoursMark);
