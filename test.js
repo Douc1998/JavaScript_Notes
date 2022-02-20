70d2b174ca2d3ab9e60f8f8efe065f15978bb680
Douc1998 <cugdc@cug.edu.cn>
2022-02-20 21:16:42 +0800

    function update

--- test.js
+++ test.js
@@ -1,10 +1,5 @@
-// 利用函数声明
-console.log(sum(10, 10)); // 20
-function sum(num1, num2){
-  return num1 + num2;
-}
-// 利用函数表达式
-console.log(sum(10, 10)); // ReferenceError: Cannot access 'sum' before initialization
-let sum = function(num1, num2){
-  return num1 + num2;
-}
+(function(){
+  for(let i = 0; i < 6; i++){
+    console.log(i)
+  }
+})(); // 0 1 2 3 4 5
