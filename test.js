9ba5da36077a9379dcafe9aa6fe781fb2a7b6046
Douc1998 <cugdc@cug.edu.cn>
2022-02-23 19:45:23 +0800

    update asynchronous

--- test.js
+++ test.js
@@ -1,5 +1,8 @@
-(function(){
-  for(let i = 0; i < 6; i++){
-    console.log(i)
-  }
-})(); // 0 1 2 3 4 5
+let x = 520;
+// 延时函数, 1s后执行
+setTimeout(() => {
+  x += 1;
+  console.log(x); // 521
+}, 1000)
+
+console.log(x) //520
