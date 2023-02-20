2252e0b8a969cc574aa2477744ec0671810954d7
Douc1998 <cugdc@cug.edu.cn>
2023-02-20 19:56:39 +0800

    update XHR

--- Test/test.js
+++ Test/test.js
@@ -1,14 +1,14 @@
-// let xhr = new XMLHttpRequest();
-// xhr.open('GET', './myData/users.json', true);
-// xhr.responseText = 'json';
-// xhr.onload = function(){
-//     if((this.status >= 200 && this.status < 300) || this.status == 304){
-//         console.log(this.response);
-//     }else{
-//         console.log(new Error(this.statusText));
-//     }
-// }
-// xhr.send();
+let xhr = new XMLHttpRequest();
+xhr.open('GET', './myData/users.json', true);
+xhr.responseText = 'json';
+xhr.onload = function(){
+    if((this.status >= 200 && this.status < 300) || this.status == 304){
+        console.log(this.response);
+    }else{
+        console.log(new Error(this.statusText));
+    }
+}
+xhr.send();
 
 // ！！！ 使用 GET 就可以获取成功，但是 POST 不行。原因是：静态资源只能 GET，不能 POST
 // let xhr = new XMLHttpRequest();
